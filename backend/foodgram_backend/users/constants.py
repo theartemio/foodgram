52dbf9d84b5b10916b199167f3abaff8b3ab2615
Artemii Tretiakov <artemii.tretiakov@gmail.com>
2024-11-13 14:37:45 +0300

    Initial. Custom user model added

---
+++ backend/foodgram_backend/users/constants.py
@@ -0,0 +1,23 @@
+# Maximum length for emails
+MAX_EMAIL_LENGTH = 254
+
+# Maximum length for names of all kinds
+MAX_NAMES_LENGTH = 150
+
+# Maximum role length
+MAX_ROLE_LENGTH = 16
+
+# Admin role
+ADMIN = "admin"
+
+# User role
+USER = "user"
+
+# User roles
+CHOICES = (
+    (USER, USER),
+    (ADMIN, ADMIN),
+)
+
+# Username allowed symbols
+USERNAME_PATTERN = r"^[\w.@+-]+\Z"
---
+++ backend/foodgram_backend/users/models.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.models import AbstractBaseUser
+
+from django.contrib.auth.models import PermissionsMixin
+
+from django.db import models
+
+from .constants import (
+    MAX_EMAIL_LENGTH,
+    MAX_NAMES_LENGTH,
+    MAX_ROLE_LENGTH,
+    CHOICES,
+    USERNAME_PATTERN,
+    USER,
+)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(
+        max_length=MAX_EMAIL_LENGTH,
+        unique=True,
+        verbose_name="Эмейл",
+        help_text="Эмейл для входа и получения писем",
+    )
+    username = models.CharField(
+        max_length=MAX_NAMES_LENGTH,
+        unique=True,
+        verbose_name="Юзернейм",
+        help_text="Уникальный юзернейм (никнейм, псевдоним).",
+    )
+    first_name = models.CharField(
+        max_length=MAX_NAMES_LENGTH,
+        verbose_name="Имя",
+        help_text="Имя. Полное или ваше любимое сокращение, например 'Васёк'.",
+    )
+    last_name = models.CharField(
+        max_length=MAX_NAMES_LENGTH,
+        verbose_name="Фамилия",
+        help_text="Фамилия. Настоящая или выдуманная.",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars/", null=True, default=None
+    )
+    role = models.CharField(
+        max_length=MAX_ROLE_LENGTH,
+        choices=CHOICES,
+        default=USER,
+        verbose_name="Пользовательская роль.",
+        help_text=(
+            "Пользователь может быть либо",
+            " обычным юзером, либо админом.",
+        ),
+    )
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    USERNAME_FIELD = "email"
