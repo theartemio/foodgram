c8b0a77e1d7ea49a67a0357ecc318090c2b248b2
Artemii Tretiakov <artemii.tretiakov@gmail.com>
2024-11-28 12:25:27 +0300

    deleted excessive permisson

--- backend/foodgram_backend/users/permissions.py
+++ backend/foodgram_backend/users/permissions.py
@@ -1,7 +1,6 @@
 from foodgram_backend.constants import ADMIN
 from rest_framework import permissions
 
-#
 class IsAuthorOrReadOnly(permissions.BasePermission):
     """Проверяет, что пользователь залогинен и он - автор записи."""
 
@@ -17,23 +16,8 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
             or obj.author == request.user
         )
 
-class IsAdminOrReadonly(permissions.BasePermission):
-    """
-    Пермишен для админа, обеспечивает доступ для
-    изменения только админу, остальным ролям и анонимным
-    пользователям доступен просмотр
-    """
-
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-            and request.user.role == ADMIN
-        )
-
-#
 class IsSameUserOrAdmin(permissions.BasePermission):
-    """Проверяет, что пользователь залогинен и запрашивает записи о себе."""
+    """Проверяет, что пользователь запрашивает записи о себе или является админом."""
 
     def has_permission(self, request, view):
         return True
@@ -45,35 +29,6 @@ class IsSameUserOrAdmin(permissions.BasePermission):
             or request.user.role == ADMIN)
         )
 
-# Используется
-class IsSameUserOrRestricted(permissions.BasePermission):
-    """Проверяет, что пользователь залогинен и запрашивает записи о себе."""
-
-    def has_permission(self, request, view):
-        return True
-
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            and request.user.is_authenticated
-        )
-
-class IsSameUserOrReadOnly(permissions.BasePermission):
-    """Проверяет, что пользователь залогинен и он - автор записи."""
-
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
-
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or obj.user == request.user
-        )
-
-# 
 class IsSameUserOrRestricted(permissions.BasePermission):
     """Проверяет, что пользователь залогинен и запрашивает записи о себе."""
 
